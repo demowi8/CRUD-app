a5cf4581569379b1e77a0adb588a8c3f23c2f8eb
Desmond Williams <demowi8@gmail.com>
2022-05-06 00:55:32 -0400

    started js

---
+++ main.js
@@ -0,0 +1,19 @@
+var selectedRow = null;
+function onFormSubmit(){
+
+}
+
+//Retrieve the Data
+function readFormData(){
+    var formData = {};
+    formData[""] = document.getElementById("").value;
+    formData[""] = document.getElementById("").value;
+    formData[""] = document.getElementById("").value;
+    formData[""] = document.getElementById("").value;
+    formData[""] = document.getElementById("").value;
+    formData[""] = document.getElementById("").value;
+    formData[""] = document.getElementById("").value;
+    formData[""] = document.getElementById("").value;
+    formData[""] = document.getElementById("").value;
+
+}
