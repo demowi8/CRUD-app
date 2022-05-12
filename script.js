67edcea66aa07025c32e20603105c0ccf58f1802
Desmond Williams <demowi8@gmail.com>
2022-05-12 17:21:06 -0400

    working code!

--- script.js
+++ script.js
@@ -2,13 +2,13 @@
     var selectedRow = null;
     
     function onFormSubmit(e){
-        e.preventDefault();
-    var formDatas = readFormData();
+        event.preventDefault();
+    let formData = readFormData();
     if(selectedRow == null){
-        insertNewRecord(formDatas);
+        insertNewRecord(formData);
     }
     else{
-        updateRecord(formDatas);
+        updateRecord(formData);
     }
     resetForm();
 };
@@ -19,7 +19,7 @@ function readFormData(){
     formData["firstName"] = document.getElementById("firstName").value;
     formData["middleName"] = document.getElementById("middleName").value;
     formData["lastName"] = document.getElementById("lastName").value;
-    formData["phoneNumber"] = document.getElementById("phone").value;
+    formData["phoneNumber"] = document.getElementById("phoneNumber").value;
     formData["email"] = document.getElementById("email").value;
     formData["address"] = document.getElementById("address").value;
     formData["dob"] = document.getElementById("dob").value;
@@ -35,38 +35,38 @@ function insertNewRecord(data){
     var table = document.getElementById('storedList').getElementsByTagName('tbody')[0];
     var newRow = table.insertRow(table.length);
 
-    var cell0 = newRow.insertCell(0);
+     cell0 = newRow.insertCell(0);
         cell0.innerHTML = data.firstName;
         
-    var cell1 = newRow.insertCell(1);
+     cell1 = newRow.insertCell(1);
         cell1.innerHTML = data.middleName;
         
-    var cell2 = newRow.insertCell(2);
+     cell2 = newRow.insertCell(2);
         cell2.innerHTML = data.lastName;
         
-    var cell3 = newRow.insertCell(3);
+     cell3 = newRow.insertCell(3);
         cell3.innerHTML = data.phoneNumber;
 
-    var cell4 = newRow.insertCell(4);
+     cell4 = newRow.insertCell(4);
         cell4.innerHTML = data.email;
         
-    var cell5 = newRow.insertCell(5);
+     cell5 = newRow.insertCell(5);
         cell5.innerHTML = data.address;
         
-    var cell6 = newRow.insertCell(6);
+     cell6 = newRow.insertCell(6);
         cell6.innerHTML = data.dob;
     
-    var cell7 = newRow.insertCell(7);
+     cell7 = newRow.insertCell(7);
     cell7.innerHTML = data.city;
     
-    var cell8 = newRow.insertCell(8);
+     cell8 = newRow.insertCell(8);
         cell8.innerHTML = data.state;
         
-    var cell9 = newRow.insertCell(9);
+     cell9 = newRow.insertCell(9);
         cell9.innerHTML = data.zip;
         
-    var cell10 = newRow.insertCell(10);
-        cell10.innerHTML = '<button>Edit</button> <button>Delete</button>'
+     cell10 = newRow.insertCell(10);
+        cell10.innerHTML = `<button onClick="onEdit(this)">Edit</button> <button onClick="onDelete(this)">Delete</button>`;
     };
     
     //Edit the data
